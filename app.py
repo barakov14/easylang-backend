34a182c9b4f4518d7933f18460ee1877c127183b
barakov14 <playmagma4@gmail.com>
2024-05-01 15:33:14 +0500

    CORS

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def create_app(db_url=None):
     # Создание API
     api = Api(app)
 
-    CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})
+    CORS(app, resources={r"/*": {"origins": "https://sdu-easylang-project.web.app/"}})
 
     # Флаг для отслеживания, была ли уже выполнена инициализация базы данных
     initialized = False
